7a3378475a50e20f03b735d6f5970579f73d4712
Pablo Cerda <pablo@citla.com>
2020-09-11 13:36:11 +0200

    Parse trading view signal and log to console

--- handler.ts
+++ handler.ts
@@ -3,15 +3,14 @@ import { Handler } from "aws-lambda";
 export const trading_view_strategy_signal: Handler = (event: any) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify({
+      message: "OK",
+    }),
   };
+
+  const payload = event.body || "{}";
+  const signalData = JSON.parse(payload);
+  console.log("TV Signal: ", signalData);
 
   return new Promise((resolve) => {
     resolve(response);
